9791613f2b14a50a963caa5da6c13d52780bebe5
sudo-technoir <skurinskayae@gmail.com>
2025-03-15 22:11:24 +0300

    Update main.cpp

--- tasks/seq/shkurinskaya_e_bin_labeling/func_tests/main.cpp
+++ tasks/seq/shkurinskaya_e_bin_labeling/func_tests/main.cpp
@@ -118,3 +118,67 @@ TEST(shkurinskaya_e_bin_labeling_seq, test_two_components) {
   test_task_sequential.PostProcessing();
   ASSERT_EQ(ans, out);
 }
+
+TEST(shkurinskaya_e_bin_labeling_seq, test_horizontal_stripe) {
+  int height = 100;
+  int width = 100;
+  int size = width * height;
+  // Create data
+  std::vector<int> in(size);
+  std::vector<int> out(size);
+  std::vector<int> ans(size);
+  for (int j = 0; j < width; ++j) {
+    in[j] = 1;
+    ans[j] = 1;
+  }
+  // Create TaskData
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  task_data_seq->inputs_count.emplace_back(in.size());
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(&height));
+  task_data_seq->inputs_count.emplace_back(1);
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(&width));
+  task_data_seq->inputs_count.emplace_back(1);
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  task_data_seq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  shkurinskaya_e_bin_labeling::TestTaskSequential test_task_sequential(task_data_seq);
+  ASSERT_EQ(test_task_sequential.Validation(), true);
+  test_task_sequential.PreProcessing();
+  test_task_sequential.Run();
+  test_task_sequential.PostProcessing();
+  ASSERT_EQ(ans, out);
+}
+
+TEST(shkurinskaya_e_bin_labeling_seq, test_vertical_stripe) {
+  int height = 100;
+  int width = 100;
+  int size = width * height;
+  // Create data
+  std::vector<int> in(size);
+  std::vector<int> out(size);
+  std::vector<int> ans(size);
+  for (int j = 0; j < height; ++j) {
+    in[j * width] = 1;
+    ans[j * width] = 1;
+  }
+  // Create TaskData
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  task_data_seq->inputs_count.emplace_back(in.size());
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(&height));
+  task_data_seq->inputs_count.emplace_back(1);
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(&width));
+  task_data_seq->inputs_count.emplace_back(1);
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  task_data_seq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  shkurinskaya_e_bin_labeling::TestTaskSequential test_task_sequential(task_data_seq);
+  ASSERT_EQ(test_task_sequential.Validation(), true);
+  test_task_sequential.PreProcessing();
+  test_task_sequential.Run();
+  test_task_sequential.PostProcessing();
+  ASSERT_EQ(ans, out);
+}
