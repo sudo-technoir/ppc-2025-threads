71d2704821d1adc25f24103e143c9833c558d5cb
sudo-technoir <skurinskayae@gmail.com>
2025-04-05 00:43:13 +0300

    Add files via upload

---
+++ tasks/omp/shkurinskaya_e_bin_labeling/include/ops_omp.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <utility>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace shkurinskaya_e_bin_labeling_omp {
+
+class TaskOMP : public ppc::core::Task {
+ public:
+  explicit TaskOMP(ppc::core::TaskDataPtr task_data) : Task(std::move(task_data)) {}
+  bool PreProcessingImpl() override;
+  bool ValidationImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+
+ private:
+  int width_, height_;
+  std::vector<int> input_;
+  std::vector<int> res_;
+  std::vector<int> parent_;
+  std::vector<int> label_;
+  std::vector<int> rank_;
+  void unionSets(int index_a, int index_b);
+  int findRoot(int index);
+};
+
+}  // namespace shkurinskaya_e_bin_labeling_omp
