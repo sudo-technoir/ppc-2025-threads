f2814a3b207a96fd8e38495f8cc05de68a90b726
sudo-technoir <skurinskayae@gmail.com>
2025-04-25 20:17:30 +0300

    Create ops_tbb.hpp

---
+++ tasks/tbb/shkurinskaya_e_bin_labeling/include/ops_tbb.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <utility>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace shkurinskaya_e_bin_labeling_tbb {
+
+class TaskTBB : public ppc::core::Task {
+ public:
+  explicit TestTaskTBB(ppc::core::TaskDataPtr task_data) : Task(std::move(task_data)) {}
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+
+ private:
+  int width_ = 0;
+  int height_ = 0;
+  std::vector<int> input_;
+  std::vector<int> res_;
+  std::vector<int> parent_;
+  std::vector<int> rank_;
+  std::vector<int> label_;
+
+  int FindRoot(int index);
+  void UnionSets(int a, int b);
+};
+
+}  // namespace shkurinskaya_e_bin_labeling_tbb
