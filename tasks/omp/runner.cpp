155b2bd4bd5466cca18bd53d9f702f42abaab171
sudo-technoir <skurinskayae@gmail.com>
2025-05-13 20:35:44 +0300

    Update runner.cpp

--- tasks/omp/runner.cpp
+++ tasks/omp/runner.cpp
@@ -1,6 +1,15 @@
 #include <gtest/gtest.h>
+#include <iostream>
 
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+int main(int argc, char** argv) {
+    try {
+        ::testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
+    } catch (const std::exception& e) {
+        std::cerr << "Caught std::exception: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    } catch (...) {
+        std::cerr << "Caught unknown exception." << std::endl;
+        return EXIT_FAILURE;
+    }
 }
