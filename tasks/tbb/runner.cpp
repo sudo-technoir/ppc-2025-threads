42e6abdfa0dbc694e660746b1fdd186f778dac1d
sudo-technoir <skurinskayae@gmail.com>
2025-05-13 21:24:04 +0300

    Update runner.cpp

--- tasks/tbb/runner.cpp
+++ tasks/tbb/runner.cpp
@@ -8,6 +8,14 @@ int main(int argc, char** argv) {
   // Limit the number of threads in TBB
   tbb::global_control control(tbb::global_control::max_allowed_parallelism, ppc::util::GetPPCNumThreads());
 
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  try {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+  } catch (const std::exception& e) {
+    std::cerr << "Caught std::exception: " << e.what() << std::endl;
+    return EXIT_FAILURE;
+  } catch (...) {
+    std::cerr << "Caught unknown exception." << std::endl;
+    return EXIT_FAILURE;
+  }
 }
