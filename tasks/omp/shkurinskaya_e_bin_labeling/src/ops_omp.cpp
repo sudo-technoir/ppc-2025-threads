41749ed71e646b3dc04693c1550cc14e470ffd7c
sudo-technoir <skurinskayae@gmail.com>
2025-05-25 17:01:14 +0300

    Update ops_omp.cpp

--- tasks/omp/shkurinskaya_e_bin_labeling/src/ops_omp.cpp
+++ tasks/omp/shkurinskaya_e_bin_labeling/src/ops_omp.cpp
@@ -133,4 +133,4 @@ void TaskOMP::ProcessUnion() {
 
   bool TaskOMP::IsValidIndex(int i, int j) const { return (i >= 0 && i < height_ && j >= 0 && j < width_); }
 
-} // namespace shkurinskaya_e_bin_labeling_omp
+}  // namespace shkurinskaya_e_bin_labeling_omp
