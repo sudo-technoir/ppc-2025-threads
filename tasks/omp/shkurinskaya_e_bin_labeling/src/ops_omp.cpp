48df4ee9d7d1763a015d9cf16c1828b746e469ee
sudo-technoir <skurinskayae@gmail.com>
2025-05-29 20:09:25 +0300

    Update ops_omp.cpp

--- tasks/omp/shkurinskaya_e_bin_labeling/src/ops_omp.cpp
+++ tasks/omp/shkurinskaya_e_bin_labeling/src/ops_omp.cpp
@@ -35,21 +35,17 @@ bool shkurinskaya_e_bin_labeling_omp::TaskOMP::IsValidIndex(int i, int j) const 
   return (i >= 0 && i < height_ && j >= 0 && j < width_);
 }
 
-void shkurinskaya_e_bin_labeling_omp::TaskOMP::UnionSets(int a, int b) {
+void TaskOMP::UnionSets(int a, int b) {
   int rootA = FindRoot(a);
   int rootB = FindRoot(b);
+
   if (rootA != rootB) {
 #pragma omp critical(union_lock)
     {
-      // Повторная проверка после входа в критическую секцию
-      rootA = FindRoot(rootA);
-      rootB = FindRoot(rootB);
-      if (rootA != rootB) {
-        if (rank_[rootA] < rank_[rootB]) std::swap(rootA, rootB);
-        parent_[rootB] = rootA;
-        if (rank_[rootA] == rank_[rootB]) {
-          rank_[rootA]++;
-        }
+      if (rank_[rootA] < rank_[rootB]) std::swap(rootA, rootB);
+      parent_[rootB] = rootA;
+      if (rank_[rootA] == rank_[rootB]) {
+        rank_[rootA]++;
       }
     }
   }
