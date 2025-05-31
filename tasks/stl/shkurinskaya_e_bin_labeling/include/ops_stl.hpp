9e3f0f08e4189c487c7f7ff44d866289e8a2639a
sudo-technoir <skurinskayae@gmail.com>
2025-06-01 01:27:40 +0300

    Update ops_stl.hpp

--- tasks/stl/shkurinskaya_e_bin_labeling/include/ops_stl.hpp
+++ tasks/stl/shkurinskaya_e_bin_labeling/include/ops_stl.hpp
@@ -33,7 +33,7 @@ class TaskSTL : public ppc::core::Task {
   int FindRoot(int index);
   void UnionSets(int a, int b);
   void InitializeUFRange(int row_start, int row_end);
-  void ProcessUnionRange(int row_start, int row_end);
+  [[nodiscard]] void ProcessUnionRange(int row_start, int row_end);
   void CompressPathsRange(int row_start, int row_end);
 
   int NumThreads() const { return ppc::util::GetPPCNumThreads(); }
