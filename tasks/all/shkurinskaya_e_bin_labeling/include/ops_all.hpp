4a574a48ac3baace2bc3302dc6791dda4ac56382
sudo-technoir <skurinskayae@gmail.com>
2025-06-01 17:51:36 +0300

    Update ops_all.hpp

--- tasks/all/shkurinskaya_e_bin_labeling/include/ops_all.hpp
+++ tasks/all/shkurinskaya_e_bin_labeling/include/ops_all.hpp
@@ -24,6 +24,11 @@ class TaskMPITBB : public ppc::core::Task {
   std::vector<int> res_local_;
   std::vector<int> counts_, displs_;
   std::vector<int> input_global_;
+  std::vector<int> parent_;
+  std::vector<int> rank_;
+  std::vector<int> label_;
+  tbb::spin_mutex uf_mutex_;
+
   int width_, height_;
   int local_H_, local_offset_rows_;
 
