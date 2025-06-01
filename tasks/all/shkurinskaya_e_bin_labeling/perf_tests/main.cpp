e7704cc3c379dcb3d0602add99a699b1fc1c1356
sudo-technoir <skurinskayae@gmail.com>
2025-06-01 18:52:20 +0300

    Create main.cpp

---
+++ tasks/all/shkurinskaya_e_bin_labeling/perf_tests/main.cpp
@@ -0,0 +1,102 @@
+#include <gtest/gtest.h>
+
+#include <chrono>
+#include <cstdint>
+#include <memory>
+#include <vector>
+
+#include "all/shkurinskaya_e_bin_labeling/include/ops_all.hpp"
+#include "core/perf/include/perf.hpp"
+#include "core/task/include/task.hpp"
+
+TEST(shkurinskaya_e_bin_labeling_all, test_pipeline_run) {
+  boost::mpi::communicator world;
+  int height = 5000;
+  int width = 5000;
+  int size = width * height;
+  // Create data
+  std::vector<int> in(size, 1);
+  std::vector<int> out(size);
+  std::vector<int> ans(size, 1);
+  // Create TaskData
+  if (world.rank() == 0) {
+    auto task_data_all = std::make_shared<ppc::core::TaskData>();
+    task_data_all->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+    task_data_all->inputs_count.emplace_back(in.size());
+    task_data_all->inputs.emplace_back(reinterpret_cast<uint8_t *>(&height));
+    task_data_all->inputs_count.emplace_back(1);
+    task_data_all->inputs.emplace_back(reinterpret_cast<uint8_t *>(&width));
+    task_data_all->inputs_count.emplace_back(1);
+    task_data_all->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+    task_data_all->outputs_count.emplace_back(out.size());
+  }
+  // Create Task
+  auto task_all = std::make_shared<shkurinskaya_e_bin_labeling_all::TaskMPITBB>(task_data_all);
+
+  // Create Perf attributes
+  auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
+  perf_attr->num_running = 10;
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perf_attr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
+
+  // Create and init perf results
+  auto perf_results = std::make_shared<ppc::core::PerfResults>();
+
+  // Create Perf analyzer
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(task_all);
+  perf_analyzer->PipelineRun(perf_attr, perf_results);
+  if (world.rank() == 0) {
+    ppc::core::Perf::PrintPerfStatistic(perf_results);
+    ASSERT_EQ(ans, out);
+  }
+}
+
+TEST(shkurinskaya_e_bin_labeling_all, test_task_run) {
+  boost::mpi::communicator world;
+  int height = 5000;
+  int width = 5000;
+  int size = width * height;
+  // Create data
+  std::vector<int> in(size, 1);
+  std::vector<int> out(size);
+  std::vector<int> ans(size, 1);
+  // Create TaskData
+  if (world.rank() == 0) {
+    auto task_data_all = std::make_shared<ppc::core::TaskData>();
+    task_data_all->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+    task_data_all->inputs_count.emplace_back(in.size());
+    task_data_all->inputs.emplace_back(reinterpret_cast<uint8_t *>(&height));
+    task_data_all->inputs_count.emplace_back(1);
+    task_data_all->inputs.emplace_back(reinterpret_cast<uint8_t *>(&width));
+    task_data_all->inputs_count.emplace_back(1);
+    task_data_all->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+    task_data_all->outputs_count.emplace_back(out.size());
+  }
+  // Create Task
+  auto task_all = std::make_shared<shkurinskaya_e_bin_labeling_all::TaskMPITBB>(task_data_all);
+
+  // Create Perf attributes
+  auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
+  perf_attr->num_running = 10;
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perf_attr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
+
+  // Create and init perf results
+  auto perf_results = std::make_shared<ppc::core::PerfResults>();
+
+  // Create Perf analyzer
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(task_all);
+  perf_analyzer->PipelineRun(perf_attr, perf_results);
+  if (world.rank() == 0) {
+    ppc::core::Perf::PrintPerfStatistic(perf_results);
+    ASSERT_EQ(ans, out);
+  }
+}
