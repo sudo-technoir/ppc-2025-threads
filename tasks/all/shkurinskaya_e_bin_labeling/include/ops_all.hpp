1639e30737c74adaf1a45576fb2baaa1a7146bb5
sudo-technoir <skurinskayae@gmail.com>
2025-06-01 18:14:30 +0300

    Update ops_all.hpp

--- tasks/all/shkurinskaya_e_bin_labeling/include/ops_all.hpp
+++ tasks/all/shkurinskaya_e_bin_labeling/include/ops_all.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
 #include <boost/mpi/communicator.hpp>
+#include <tbb/spin_mutex.h>
 #include <cstddef>
 #include <functional>
 #include <utility>
 #include <vector>
 
 #include "core/task/include/task.hpp"
+#include "core/util/include/util.hpp"
 
 namespace shkurinskaya_e_bin_labeling_all {
 
